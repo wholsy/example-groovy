d2fd3ff4e42355901c36cbfd9cafd7192a24a89d
yueny <yueny09@163.com>
2020-08-13 21:56:20 +0800

    h

--- src/main/java/com/yueny/example/groovy/BootApplicationstrap.java
+++ src/main/java/com/yueny/example/groovy/BootApplicationstrap.java
@@ -1,10 +1,11 @@
 package com.yueny.example.groovy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,20 +15,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author yueny09 <deep_blue_yang@163.com>
  * @DATE 2017年3月9日 下午5:53:29
  */
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan(basePackages = { "com.yueny.example.groovy" })
-public class Bootstrap {
+public class BootApplicationstrap {
 	/**
 	 * 日志记录器
 	 */
-	private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+	private static final Logger logger = LoggerFactory.getLogger(BootApplicationstrap.class);
 
-	public static void main(final String[] args) {
-		try {
-			SpringApplication.run(Bootstrap.class, args);
-		} catch (final Exception e) {
-			logger.error("服务启动异常:", e);
+	public static void main(String[] args) {
+		try{
+			ApplicationContext context = SpringApplication.run(BootApplicationstrap.class, args);
+			String serverPort = context.getEnvironment().getProperty("server.port");
+			log.info("mblog started at http://localhost:" + serverPort);
+		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
